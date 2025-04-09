f0e39d048aa9842ff441e70470895d69addd8265
Matti Luukkainen <mluukkai@iki.fi>
2025-04-09 19:21:17 +0300

    broadcaster

--- todo-broadcaster/index.js
+++ todo-broadcaster/index.js
@@ -1,7 +1,11 @@
 
 const { connect } = require("@nats-io/transport-node");
+const { Webhook, MessageBuilder } = require('discord-webhook-node');
 
 const NATS_URI = process.env.NATS_URI || 'nats://localhost:4222';
+const WEBHOOK = process.env.WEBHOOK || 'https://study.cs.helsinki.fi/discord/webhooks/1264842173619109949';
+
+const hook = new Webhook(WEBHOOK);
 
 const main =  async () => {
   console.log(`Broadcaster runnig`);
@@ -10,8 +14,27 @@ const main =  async () => {
   const sub = nc.subscribe("todo");
   (async () => {
     for await (const m of sub) {
-      const obj = JSON.parse(m.string());
-      console.log(obj);
+      try {
+        const object = JSON.parse(m.string());
+
+        const embed = new MessageBuilder()
+        .setAuthor('Todo app broadcaster')
+        .setTitle('DevOps with Kubernetes')
+        .addField(`todo ${object.action}`, object.todo.content)
+        .setColor('#00b0f4')
+        .setURL('https://devopswithkubernetes.com/')
+        .setTimestamp();
+        
+        await hook.send(embed);
+      }
+      catch (err) {
+        if (err.toString().includes("Error: Error sending webhook: 200 status code. Response: Webhook sent successfully")) {
+          console.log("Webhook sent successfully");
+        } else {
+          console.log(err);
+        }
+      }
+
     }
     console.log("subscription closed");
   })();
